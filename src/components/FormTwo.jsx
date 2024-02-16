a772dd1da9aa660b07762d15fa7ac6a8b1f8d604
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-02-16 18:51:34 +0530

    completed features

--- src/components/FormTwo.jsx
+++ src/components/FormTwo.jsx
@@ -1,10 +1,15 @@
 import React, { useState,useRef } from 'react'
+import ResultPage from './ResultPage'
+import { useForm } from "react-hook-form";
 
 const FormTwo = (props) => {
     const [familyMembers,setFamilyMembers]=useState([])
     const name=useRef()
     const relation=useRef()
     var id=0;
+    const form = useForm();
+    const { register} = form;
+    const [flag,setFlag]=useState(true)
 
     console.log("data 1" ,props)
     console.log("data 2",familyMembers)
@@ -15,6 +20,9 @@ const FormTwo = (props) => {
         setFamilyMembers(newList)
         console.log(newList)
         
+    }
+    function handleSubmit(){
+        setFlag(false)
     }
 
     function handleClick(){
@@ -29,54 +37,68 @@ const FormTwo = (props) => {
 
     console.log("asdsadas",familyMembers)
   return (
-    <div className="form-two bg-white p-8 rounded-md px-12">
-      <h1 className="text-3xl">Add family members</h1>
-      <div className="input flex mt-6 gap-4 justify-center items-center">
-        <div className="name">
-          <label htmlFor="name">Name :</label>
-          <input
-            ref={name}
-            type="text"
-            name=""
-            id=""
-            className=" ml-2 p-1 px-2 rounded-md border border-black"
-          />
-        </div>
-        <div className="relation ">
-          <label htmlFor="relation">Relation :</label>
-          <input
-            type="text"
-            ref={relation}
-            name=""
-            id=""
-            className=" ml-2 p-1 px-2 rounded-md border border-black"
-          />
-        </div>
-        <button
-          onClick={handleClick}
-          className="bg-green-300 p-2 rounded-md w-[100px] hover:opacity-80 mt-1"
-        >
-          Add
-        </button>
-      </div>
-      {familyMembers.map((ele, index) => (
-        <div key={id++} className="">
-          <div className="flex justify-between my-4">
-            <p className="">
-              {ele?.id}. {ele?.userName}
-            </p>
-            <p className=""> {ele?.userRelation}</p>
-            <p className="cursor-pointer" onClick={() => handleDelete(ele?.id)}>
-              {" "}
-               ❌ 
-            </p>
+    <>
+      {flag ? (
+        <div className="form-two bg-white p-8 rounded-md px-12">
+          <h1 className="text-3xl">Add family members</h1>
+          <div className="input flex mt-6 gap-4 justify-center items-center">
+            <div className="name">
+              <label htmlFor="name">Name :</label>
+              <input
+                ref={name}
+                type="text"
+                name=""
+                id=""
+                className=" ml-2 p-1 px-2 rounded-md border border-black"
+                
+              />
+            </div>
+            <div className="relation ">
+              <label htmlFor="relation">Relation :</label>
+              <input
+                type="text"
+                ref={relation}
+                name=""
+                id=""
+                className=" ml-2 p-1 px-2 rounded-md border border-black"
+              />
+            </div>
+            <button
+              onClick={handleClick}
+              className="bg-green-300 p-2 rounded-md w-[100px] hover:opacity-80 mt-1"
+            >
+              Add
+            </button>
           </div>
-          <hr className="bg-gray-800 " />
-        </div>
-      ))}
+          {familyMembers.map((ele, index) => (
+            <div key={id++} className="">
+              <div className="flex justify-between my-4">
+                <p className="">
+                  {ele?.id}. {ele?.userName}
+                </p>
+                <p className=""> {ele?.userRelation}</p>
+                <p
+                  className="cursor-pointer"
+                  onClick={() => handleDelete(ele?.id)}
+                >
+                  {" "}
+                  ❌
+                </p>
+              </div>
+              <hr className="bg-gray-800 " />
+            </div>
+          ))}
 
-      {familyMembers.length!=0 && <button className=" bg-green-300 p-2 rounded-md w-full  hover:opacity-80 mt-6">Submit </button>}
-    </div>
+          {familyMembers.length != 0 && (
+            <button className=" bg-green-300 p-2 rounded-md w-full  hover:opacity-80 mt-6" onClick={handleSubmit}>
+              Submit{" "}
+            </button>
+          )}
+        </div>
+      ) : (
+        <ResultPage formOneData={props} formTwoData={familyMembers}></ResultPage>
+      )}
+    </>
   );
 }
 
---
+++ src/components/ResultPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ResultPage = (props) => {
+  console.log("Result page",props)
+    return (
+      <div>
+        <h1 className="text-3xl mb-1 ">Succesfully added all the details !</h1>
+        <div className="flex flex-col gap-2">
+          <p>First name : {props?.formOneData?.formData?.firstName}</p>
+          <p>Last name : {props?.formOneData?.formData?.lastName}</p>
+          <p>Parent name : {props?.formOneData?.formData?.parentName}</p>
+          <p>Phone number : {props?.formOneData?.formData?.phoneNumber}</p>
+          <p>Email : {props?.formOneData?.formData?.Email}</p>
+          <p>Address : {props?.formOneData?.formData?.Address}</p>
+        </div>
+        <hr />
+        <h1 className="text-2xl mt-6 mb-1 ">Family members</h1>
+        <div className="flex flex-col gap-2">
+          {props?.formTwoData?.map((ele) => (
+            <div className="flex gap-4">
+              <p>{ele?.userName} :</p>
+              <p>{ele?.userRelation}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+}
+
+export default ResultPage
